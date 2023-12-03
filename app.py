bac72da36cc2727d5e9c57b2e9600c513216d479
henritaing <henri.taing@centrale-marseille.fr>
2023-12-03 11:29:44 +0100

    login

--- app.py
+++ app.py
@@ -11,6 +11,9 @@ from flask import Response
 import traceback 
 import requests
 import json 
+import random as rd
+import csv
+from tqdm import tqdm
 import phases.first_phase       #on sait jamais de combien de librairies on a besoin
 
 app = Flask(__name__)
@@ -40,6 +43,49 @@ def identification():
     resp = jsonify(success=success)
     return render_template("index.html")
 
+
+# Read TSV file and parse card data
+def read_card_data(file_path):
+    cards = []
+    with open(file_path, 'r', newline='') as mycsv:
+        reader = csv.DictReader(mycsv, delimiter='\t')
+        for idx, row in enumerate(reader):
+            if row['Choix'] == '':
+                break
+
+            # Check for empty strings and convert to integers
+            def convert_to_int(value):
+                return int(value) if isinstance(value, str) and value.strip() else value
+
+            card = {
+                'index': idx,
+                'name': row['Choix'],
+                'effect_yes_R': convert_to_int(row.get('Oui_R', 0)),
+                'effect_yes_C': convert_to_int(row.get('Oui_C', 0)),
+                'effect_yes_I': convert_to_int(row.get('Oui_I', 0)),
+                'effect_no_R': convert_to_int(row.get('Non_R', 0)),
+                'effect_no_C': convert_to_int(row.get('Non_C', 0)),
+                'effect_no_I': convert_to_int(row.get('Non_I', 0)),
+                'fire_defense': convert_to_int(row.get('feu', 0)),
+                'earth_defense': convert_to_int(row.get('terre', 0)),
+                'air_defense': convert_to_int(row.get('air', 0)),
+                'water_defense': convert_to_int(row.get('eau', 0)),
+                'intro': row.get('intro', ''),
+                'outro': row.get('outro', '')
+            }
+
+            cards.append(card)
+
+    return cards
+
+
+
+
+# Flask endpoint to serve card data
+@app.route('/api/cards')
+def get_cards():
+    card_data = read_card_data('./static/cartes_action.tsv')
+    return jsonify({'cards': card_data})
     
 if __name__ == '__main__':
     app.run(port=5000)
