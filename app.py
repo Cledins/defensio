655f813a93f73c39899ab50e8f706f794a4197e2
Cledins <cassandra49130@gmail.com>
2023-12-04 09:22:49 +0100

    quelques optimisations en back

--- app.py
+++ app.py
@@ -16,6 +16,43 @@ import csv
 from tqdm import tqdm
 import phases.first_phase       #on sait jamais de combien de librairies on a besoin
 
+# Check for empty strings and convert to integers
+def convert_to_int(value):
+    return int(value) if isinstance(value, str) and value.strip() else value
+
+# Read TSV file and parse card data
+def read_card_data(file_path):
+    cards = []
+    with open(file_path, 'r', newline='') as mycsv:
+        reader = csv.DictReader(mycsv, delimiter='\t')
+        for idx, row in enumerate(reader):
+            if row['Choix'] == '':
+                break
+
+            card = {
+                'index': idx,
+                'name': row['Choix'],
+                'effect_yes_R': convert_to_int(row.get('Oui_R', 0)),
+                'effect_yes_C': convert_to_int(row.get('Oui_C', 0)),
+                'effect_yes_I': convert_to_int(row.get('Oui_I', 0)),
+                'effect_no_R': convert_to_int(row.get('Non_R', 0)),
+                'effect_no_C': convert_to_int(row.get('Non_C', 0)),
+                'effect_no_I': convert_to_int(row.get('Non_I', 0)),
+                'fire_defense': convert_to_int(row.get('feu', 0)),
+                'earth_defense': convert_to_int(row.get('terre', 0)),
+                'air_defense': convert_to_int(row.get('air', 0)),
+                'water_defense': convert_to_int(row.get('eau', 0)),
+                'intro': row.get('intro', ''),
+                'outro': row.get('outro', '')
+            }
+
+            cards.append(card)
+
+    return cards
+
+#Generate the cards only once
+cards = read_card_data('./static/cartes_action.tsv')
+
 app = Flask(__name__)
 CORS(app)
 
@@ -43,49 +80,10 @@ def identification():
     resp = jsonify(success=success)
     return render_template("index.html")
 
-
-# Read TSV file and parse card data
-def read_card_data(file_path):
-    cards = []
-    with open(file_path, 'r', newline='') as mycsv:
-        reader = csv.DictReader(mycsv, delimiter='\t')
-        for idx, row in enumerate(reader):
-            if row['Choix'] == '':
-                break
-
-            # Check for empty strings and convert to integers
-            def convert_to_int(value):
-                return int(value) if isinstance(value, str) and value.strip() else value
-
-            card = {
-                'index': idx,
-                'name': row['Choix'],
-                'effect_yes_R': convert_to_int(row.get('Oui_R', 0)),
-                'effect_yes_C': convert_to_int(row.get('Oui_C', 0)),
-                'effect_yes_I': convert_to_int(row.get('Oui_I', 0)),
-                'effect_no_R': convert_to_int(row.get('Non_R', 0)),
-                'effect_no_C': convert_to_int(row.get('Non_C', 0)),
-                'effect_no_I': convert_to_int(row.get('Non_I', 0)),
-                'fire_defense': convert_to_int(row.get('feu', 0)),
-                'earth_defense': convert_to_int(row.get('terre', 0)),
-                'air_defense': convert_to_int(row.get('air', 0)),
-                'water_defense': convert_to_int(row.get('eau', 0)),
-                'intro': row.get('intro', ''),
-                'outro': row.get('outro', '')
-            }
-
-            cards.append(card)
-
-    return cards
-
-
-
-
 # Flask endpoint to serve card data
 @app.route('/api/cards')
 def get_cards():
-    card_data = read_card_data('./static/cartes_action.tsv')
-    return jsonify({'cards': card_data})
+    return jsonify({'cards': cards})
     
 if __name__ == '__main__':
     app.run(port=5000)
