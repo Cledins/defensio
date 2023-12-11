a3083b38e8425f84c87b859a479cb85d25b65768
Cledins <cassandra49130@gmail.com>
2023-12-11 23:35:05 +0100

    quick fix

--- app.py
+++ app.py
@@ -94,6 +94,7 @@ def create_app():
         success=False
         if request.method=='POST':
             try:
+                id = request.json['number']
                 nouvelles_metrics = UserMetrics(
                     id=int(id),
                     jauge_i=30,
