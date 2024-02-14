aa7d3d1ac1cccfa6b46c60b952ff715c989b334f
henritaing <henri.taing@centrale-marseille.fr>
2024-02-14 18:07:17 +0100

    Update ordre variables
    Pas encore fini, je recommence le travail car la trad que j'avais faite avait tout cassé

--- database/table.py
+++ database/table.py
@@ -6,9 +6,9 @@ from sqlalchemy import Boolean, DateTime, Integer, String
 
 class UserMetrics(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    jauge_i = db.Column(db.Integer, nullable=False, default=0)
     jauge_r = db.Column(db.Integer, nullable=False, default=0)
     jauge_c = db.Column(db.Integer, nullable=False, default=0)
+    jauge_i = db.Column(db.Integer, nullable=False, default=0)
     jauge_d = db.Column(db.Integer, nullable=False, default=0)
     air_defense = db.Column(db.Integer, nullable=False, default=0)
     water_defense = db.Column(db.Integer, nullable=False, default=0)
